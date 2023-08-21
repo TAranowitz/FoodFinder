fe98d31344213617a627e8fe7b23bf52b966f62a
mikeyeunguci <91280111+mikeyeunguci@users.noreply.github.com>
2023-08-21 15:49:35 -0700

    Create intro.py

---
+++ intro.py
@@ -0,0 +1,58 @@
+
+
+def Welcome():
+    print("Welcome to Food Finder for all your food reccommendation needs!")
+    print("Let's collect some data! What type of cuisine do you enjoy?")
+    cuisine_types = [
+    "Japanese",
+    "Chinese",
+    "Italian",
+    "Indian",
+    "Mexican",
+    "French",
+    "Thai",
+    "Spanish",
+    "Greek",
+    "Korean",
+    "Turkish",
+    "Vietnamese",
+    "Lebanese",
+    "Moroccan",
+    "Brazilian",
+    "Ethiopian",
+    "Russian",
+    "Jamaican",
+    "Peruvian",
+    "German",
+    "Egyptian",
+    "Indonesian",
+    "Cuban",
+    "Malaysian",
+    "Swedish",
+    "Irish",
+    "Nigerian",
+    "South African",
+    "Ukrainian",
+    "Argentine",
+    "Australian",
+    "Canadian",
+    "Pakistani",
+    "Bangladeshi",
+    "Polish",
+    "Dutch",
+    "Iranian",
+    "Israeli",
+    "Filipino",
+    "Swiss"]
+
+
+
+
+
+
+def main():
+
+
+
+if __name__ == "__main__":
+    main()
