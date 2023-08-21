fe3e834f4b9ba60bb7d4e1b6833f81e216fe078c
Terence Aranowitz <taranowi@uci.edu>
2023-08-21 16:34:28 -0700

    changed lists to set

--- intro.py
+++ intro.py
@@ -1,6 +1,6 @@
 import sys
 
-def DataCollection():
+def DataCollection()->set:
     print("Welcome to Food Finder for all your food reccommendation needs!")
     print("Let's collect some data! What type of cuisine do you enjoy?\n")
     cuisine_types = ["Japanese","Chinese","Italian","Indian","Mexican","French","Thai","Spanish","Greek","Korean","Vietnamese","Polish","Filipino"]
@@ -10,28 +10,28 @@ def DataCollection():
     
     input_str = input()
     cuisine_pref = input_str.split(" ")
-    cuisine_index = []
+    cuisine_index = set()
     for num in cuisine_pref:
         if int(num) < len(cuisine_types) and int(num) >= 0:
-            cuisine_index.append(int(num))
+            cuisine_index.add(int(num))
     
     return cuisine_index
 
-def save_cuisine_index(index_list:list) -> None:
+def save_cuisine_index(index_set:list) -> None:
     with open("saved_data.txt", "w") as file:
-        for index in index_list:
+        for index in index_set:
             file.write(f"{str(index)}\n")
 
 def load_cuisine_index()-> list:
-    return_list = []
+    return_set = set()
     with open("saved_data.txt", "r") as file:
         for line in file:
-            return_list.append(int(line))
-    return return_list
+            return_set.add(int(line))
+    return return_set
 
-def give_non_visited(index_list:list)->set:
+def give_non_visited(index_set:list)->set:
     cuisine_types = ["Japanese","Chinese","Italian","Indian","Mexican","French","Thai","Spanish","Greek","Korean","Vietnamese","Polish","Filipino"]
-    index_set = set(index_list)
+    
     non_visited = set()
     for i in range(len(cuisine_types)):
         if i not in index_set:
@@ -40,8 +40,8 @@ def give_non_visited(index_list:list)->set:
 
 
 def main():
-    #index_list = DataCollection()
-    #save_cuisine_index(index_list)
+    #index_set = DataCollection()
+    #save_cuisine_index(index_set)
     my_list = load_cuisine_index()
     print(give_non_visited(my_list))
 
