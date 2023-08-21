f323a94fb3f2bb6f6a14c68fd4aa3c993ecf1d94
mikeyeunguci <91280111+mikeyeunguci@users.noreply.github.com>
2023-08-21 16:10:37 -0700

    Update intro.py

--- intro.py
+++ intro.py
@@ -1,17 +1,25 @@
+import sys
 
-
-def Welcome():
+def DataCollection():
     print("Welcome to Food Finder for all your food reccommendation needs!")
-    print("Let's collect some data! What type of cuisine do you enjoy?")
-    cuisine_types = ["Japanese","Chinese","Italian","Indian","Mexican","French","Thai","Spanish","Greek","Korean","Turkish","Vietnamese","Lebanese","Moroccan","Brazilian","Ethiopian","Russian","Jamaican","Peruvian","German","Egyptian","Indonesian","Cuban","Malaysian","Swedish","Irish","Nigerian","South African","Ukrainian","Argentine","Australian","Canadian","Pakistani","Bangladeshi","Polish","Dutch","Iranian","Israeli","Filipino","Swiss"]
-
-
-
-
+    print("Let's collect some data! What type of cuisine do you enjoy?\n")
+    cuisine_types = ["Japanese","Chinese","Italian","Indian","Mexican","French","Thai","Spanish","Greek","Korean","Vietnamese","Polish","Filipino"]
+    for index, cuisine in enumerate(cuisine_types):
+        print(f"{index}: {cuisine}", end = " ")
+    print("Type your answers in the example format: 3 7")
+    
+    input_str = input()
+    cuisine_pref = input_str.split(" ")
+    cuisine_index = []
+    for num in cuisine_pref:
+        if int(num) < len(cuisine_types) and int(num) >= 0:
+            cuisine_index.append(int(num))
+    
+    return cuisine_index
 
 
 def main():
-
+    DataCollection()
 
 
 if __name__ == "__main__":
